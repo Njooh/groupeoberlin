20b49d2d97032be87022d50a6d487646525bb1b2
Njooh <hello@bhent.xyz>
2017-05-19 07:42:19 +0100

    Reinitialize

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,24 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery.js
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap.js
+//= require owl.carousel.js
+//= require jquery.backstretch.js
+//= require cubeportfolio.js
+//= require magnific-popup.js
+//= require custom
+//= require gmaps.js
+//= require modernizr.js
+//= require_tree .
