2870e33463ab6cfb3aa53d17e40ccae611d9f118
Njooh <hello@bhent.xyz>
2017-05-22 17:32:02 +0100

    Proble JS solve

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( owl.carousel.min.js )
+Rails.application.config.assets.precompile += %w( jquery.backstretch.min.js )
+Rails.application.config.assets.precompile += %w( cubeportfolio.min.js )
+Rails.application.config.assets.precompile += %w( magnific-popup.min.js )
+Rails.application.config.assets.precompile += %w( custom.js )
+Rails.application.config.assets.precompile += %w( gmaps.js )
+Rails.application.config.assets.precompile += %w( modernizr.min.js )
